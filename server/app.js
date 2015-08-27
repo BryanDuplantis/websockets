4be0a79781a6a0bc95b80a701a9734d916fbc99a
Bryan Duplantis <duplantis@gmail.com>
2015-08-27 14:34:24 -0500

    add socket.io dependencies

--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
 'use strict';
 
 var express = require('express');
+// server side piece
+var socketio = require('socket.io');
 
 var app = express();
 
 app.use(express.static('client'));
 
-app.listen(3000, function () {
+// create server variable, pass into below
+var server = app.listen(3000, function () {
   console.log('The magic happens on port 3000');
 });
+
+// have to pass server into socket.io
+socketio(server);
